259340e8e19af0fc7bfba26e59e0bbacee953569
Jonas <jonas.itt@tum.de>
2021-11-20 18:37:56 +0100

    Added pushing to database

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Home.css'
 import Logo from "../../ressources/Logo_Blau.png"
 import { buttons, contributionWays} from './_config'
 import $ from 'jquery'
+import { firestore } from '../../firebase'
+
+import {collection, addDoc} from 'firebase/firestore';
+//import { useCollectionData } from 'react-firebase9-hooks/firestore'
 
 export default function Home() {
+    
+    // Temporary for sending!
+    const articlesRef = collection(firestore, 'projects')
 
+    const [nam, setName] = useState('');
+    const [cat, setCat] = useState('');
+    const [desc, setDesc] = useState('');
+    const [ids, setID] = useState('');
+    const [loc, setLocation] = useState('');
+    const [sea, setSearch] = useState('');
+    const [web, setWebsite] = useState('');
+
+    const send = async(e) => {
+        e.preventDefault();
+
+        await addDoc(articlesRef, {
+            category: cat,
+            description: desc,
+            id: ids,
+            location: loc,
+            name: nam,
+            search: sea,
+            website: web,
+        })
+        
+        setID('')
+        setName('')
+        setSearch('')
+        setWebsite('')
+        setCat('')
+        setDesc('')
+        setLocation('')
+    }
+    
+    
+    
     // Changing topics
 
     function handleTopicChange(props) {
@@ -34,9 +73,16 @@ export default function Home() {
         <>
         <div className= "HomeContainer">
             <div className="WorldMap">
-                <div className="Slogan">
-                    
-                </div>
+                <form className="Inputs" onSubmit={e => send(e)}>
+                    <input value={nam}  className="inputd" id="name" type="text" placeholder="name" onChange={e => setName(e.target.value)}></input>
+                    <input value={cat} className="inputd" id="category" type="text" placeholder="category" onChange={e => setCat(e.target.value)}></input>
+                    <input value={sea} className="inputd" id="search" type="text" placeholder="Search"onChange={e => setSearch(e.target.value)}></input>
+                    <input value={loc} className="inputd" id="location" type="text" placeholder="location" onChange={e => setLocation(e.target.value)}></input>
+                    <input value={web} className="inputd" id="website" type="text" placeholder="website" onChange={e => setWebsite(e.target.value)}></input>
+                    <input value={desc} className="inputd" id="descripition" type="text" placeholder="description" onChange={e => setDesc(e.target.value)}></input>
+                    <input value={ids} className="inputd" id="id" type="text" placeholder="id" onChange={e => setID(e.target.value)}></input>
+                    <input className="inputd" id="send" type="submit" onClick={send}></input>
+                </form>
             </div> 
             <div className="InfoPanel">
                 <div className="LogoContainer">
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { getFirestore } from "@firebase/firestore";
+import {initializeApp} from "firebase/app"
+
+const app = initializeApp({
+    apiKey: "AIzaSyDFZPjeRwJU8V1g0tQk7O02GbhkB8_9pIY",
+
+    authDomain: "philopt-6f2cd.firebaseapp.com",
+  
+    projectId: "philopt-6f2cd",
+  
+    storageBucket: "philopt-6f2cd.appspot.com",
+  
+    messagingSenderId: "944091867345",
+  
+    appId: "1:944091867345:web:3695b50d729e9675e77df0",
+  
+    measurementId: "G-V9W8LNCC4H"
+  
+})
+
+export const firestore = getFirestore(app);
+export default app;
