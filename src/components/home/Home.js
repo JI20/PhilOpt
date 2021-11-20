67ff0ff9f44714a83754aa73f818f2f1bb7f39a0
Jonas <jonas.itt@tum.de>
2021-11-20 10:24:26 +0100

    Test

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,6 +5,7 @@ export default function Home() {
     return (
         <div className= "HomeContainer">
             <input className="ButtonInput Inactive" id="SearchBar" type="Button" value="Environment"/>
+            <input className="ButtonInput Inactive" id="Button" type="Button" value="Environment"/>
         </div>
     )
 }
